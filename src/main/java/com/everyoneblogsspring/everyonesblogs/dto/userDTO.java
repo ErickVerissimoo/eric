f545a6a8971ebb8f5bd69398a8ba8d8bec9930e0
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-12-07 23:34:10 -0300

    mudanças

--- src/main/java/com/everyoneblogsspring/everyonesblogs/dto/userDTO.java
+++ src/main/java/com/everyoneblogsspring/everyonesblogs/dto/userDTO.java
@@ -1,8 +1,10 @@
 package com.everyoneblogsspring.everyonesblogs.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class userDTO {
 private String email;
 private String password;
